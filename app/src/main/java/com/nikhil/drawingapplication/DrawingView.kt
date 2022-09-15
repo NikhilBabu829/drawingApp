6f065de462af55d5762521e88a9e97e2d9caccb8
Nikhil Gunti <nikhilbabu829@gmail.com>
2022-09-15 23:56:41 +0530

    allowing the brushsize to adapt itself to the screensize

--- app/src/main/java/com/nikhil/drawingapplication/DrawingView.kt
+++ app/src/main/java/com/nikhil/drawingapplication/DrawingView.kt
@@ -3,6 +3,7 @@ package com.nikhil.drawingapplication
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
@@ -25,7 +26,7 @@ class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
     //each time the size of the screen is changed
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -71,6 +72,10 @@ class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
         //
         invalidate()
         return true
+    }
+    fun setSizeForBrush(newSize : Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize,resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
     internal inner class Custompath(var color:Int,var brushThickness:Float) : Path(){
 
