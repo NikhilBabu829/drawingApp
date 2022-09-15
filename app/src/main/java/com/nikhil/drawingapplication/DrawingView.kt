b52c4b100ab574bb5b271cdb8b5b519bf96d4cb9
Nikhil Gunti <nikhilbabu829@gmail.com>
2022-09-15 22:53:33 +0530

    linking my view file to the xml file

--- app/src/main/java/com/nikhil/drawingapplication/DrawingView.kt
+++ app/src/main/java/com/nikhil/drawingapplication/DrawingView.kt
@@ -1,14 +1,10 @@
 package com.nikhil.drawingapplication
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
-import java.nio.file.Path
-
 class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
     private var mDrawPath: Custompath? = null
     private var mCanvasBitmap : Bitmap? = null
@@ -30,7 +26,46 @@ class DrawingView(context:Context,attrs:AttributeSet):View(context,attrs) {
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
     }
-    internal inner class Custompath(var color:Int,var brushThickness:Float) : Path {
+    //each time the size of the screen is changed
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
+        if (!mDrawPath!!.isEmpty){
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!,mDrawPaint!!)
+        }
+    }
+    //the below method is used to see what happens when we touch the screen
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var touchX = event?.x
+        val touchY = event?.y
+        //the below when method is where we are deciding on what is going to happen when we touch or drag the screen
+        when(event?.action){
+            MotionEvent.ACTION_DOWN->{
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!,touchY!!)
+            }
+            MotionEvent.ACTION_MOVE->{
+                mDrawPath!!.lineTo(touchX!!,touchY!!)
+            }
+            MotionEvent.ACTION_UP->{
+                mDrawPath = Custompath(color,mBrushSize)
+            }
+            else -> return false
+        }
+        //
+        invalidate()
+        return true
+    }
+    internal inner class Custompath(var color:Int,var brushThickness:Float) : Path(){
 
     }
 }
